329295f34ab13a6a2f73db9707ed516b9eff6e44
Leovison <cleovison@gmail.com>
2024-02-27 18:20:43 +0800

    .

--- src/components/NoteTaking.jsx
+++ src/components/NoteTaking.jsx
@@ -1,33 +1,45 @@
 import React, { useState } from "react";
 // add curly braces to useNotes import
-import { useNotes } from '../hooks/useNotes'
+import { useNotes } from "../hooks/useNotes";
 
 export default function NoteTaking() {
   const { notes, dispatch } = useNotes();
   const [filter, setFilter] = useState("Search");
   const [editText, setEditText] = useState(" ");
   const [editID, setEditID] = useState(null);
+  const [title, setTitle] = useState("");
 
-  const handleEdit = (id, text) => {
-    setEditID(id);
-    setEditText(text);
+  const handleSubmit = (e, title, content) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_NOTE", title, content });
+    setTitle("");
+ 
   };
+  // const handleEdit = (id, text) => {
+  //   setEditID(id);
+  //   setEditText(text);
+  // };
   return (
     <>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
         <button type="submit">Submit</button>
         <textarea name="" id="" cols="30" rows="10"></textarea>
       </form>
 
       <div className="main-content">
-        <input type="text" />
-        <ul>
-          {/* {notes.map((note) => (
-            <li></li>
-          ))} */}
-        </ul>
+   
+    
       </div>
+          <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+          
+              <span>{title}</span>
+            </li>
+            
+          ))}
+        </ul>
     </>
   );
 }
