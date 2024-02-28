3bebaaecba57e1ca5ba2da430544bc82da4dca2c
CLeovison <cleovison@gmail.com>
2024-02-28 22:32:29 +0800

    Added A Styles on Module

--- src/components/NoteTaking.jsx
+++ src/components/NoteTaking.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // add curly braces to useNotes import
 import { useNotes } from "../hooks/useNotes";
-
+import styles from "../components/mains.module.css"
 export default function NoteTaking() {
   const { notes, dispatch } = useNotes();
   const [filter, setFilter] = useState("Search");
@@ -15,23 +15,32 @@ export default function NoteTaking() {
     dispatch({ type: "ADD_NOTE", title, content });
     setTitles("");
     setTextArea(" ");
-    console.log(`${titles}`);
-    console.log(`${textArea}`);
+
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.forms}>
         <input
           type="text"
           value={titles}
           onChange={(e) => setTitles(e.target.value)}
         />
-        <button type="submit">Submit</button>
+     
         <textarea
           value={textArea}
+          className={styles.textarea}
           onChange={(e) => setTextArea(e.target.value)}
         ></textarea>
+           <button type="submit">Submit</button>
       </form>
+
+      <div className={styles.maincontent}>
+        <ul>
+          {notes.map(note =>(
+            <li key={note.id}></li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
