d0ed6354849afd5b63884653f77590351595366e
Leovison <cleovison@gmail.com>
2024-03-01 18:44:41 +0800

    Fixing the Error

--- src/components/NoteTaking.jsx
+++ src/components/NoteTaking.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // add curly braces to useNotes import
 import { useNotes } from "../hooks/useNotes";
 import styles from "../components/mains.module.css";
+
 export default function NoteTaking() {
   const { notes, dispatch } = useNotes();
   const [filter, setFilter] = useState("Search");
@@ -10,20 +11,21 @@ export default function NoteTaking() {
   const [titles, setTitles] = useState("");
   const [textArea, setTextArea] = useState("");
 
-  const handleSubmit = (e, title, content) => {
+
+  const handleEdit = (id, text, content) => {
+    setTitles(text);
+    setEditID(id);
+    setTextArea(content);
+  };
+  const handleSubmit = (e,title,content) => {
     e.preventDefault();
     dispatch({ type: "ADD_NOTE", title, content });
     setTitles("");
     setTextArea("");
   };
-  const handleEdit = (id, text, content) =>{
-      setTitles(text)
-      setEditID(id)
-      setTextArea(content)
-  }
   return (
     <>
-      <form onSubmit={handleSubmit} className={styles.forms}>
+      <form className={styles.forms}>
         <input
           type="text"
           value={titles}
@@ -35,14 +37,16 @@ export default function NoteTaking() {
           className={styles.textarea}
           onChange={(e) => setTextArea(e.target.value)}
         ></textarea>
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleSubmit} >Submit</button>
       </form>
 
       <div className={styles.maincontent}>
         <ul>
           {notes.map((note) => (
-            <li key={note?.id}>
-              {editID !== note.id && <label>{note.title}</label>}
+            <li key={note.id}>
+              {editID !== note.id && (
+                <label htmlFor={note.id}>{note.title}</label>
+              )}
             </li>
           ))}
         </ul>
