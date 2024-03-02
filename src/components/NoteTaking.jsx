8ed4ceed68e564c38146250daaa819bc4337fec6
CLeovison <cleovison@gmail.com>
2024-03-02 23:07:40 +0800

    Doneeee! Fuck this Shit!

--- src/components/NoteTaking.jsx
+++ src/components/NoteTaking.jsx
@@ -11,43 +11,52 @@ export default function NoteTaking() {
   const [titles, setTitles] = useState("");
   const [textArea, setTextArea] = useState("");
 
-
   const handleEdit = (id, text, content) => {
     setTitles(text);
     setEditID(id);
     setTextArea(content);
   };
-  const handleSubmit = (e,title,content) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: "ADD_NOTE", title, content });
+    dispatch({ type: "ADD_NOTE", title: titles, content: textArea });
     setTitles("");
     setTextArea("");
-
-
   };
+  const handleDelete = (id) =>{
+    dispatch({type: "DELETE_NOTE", id})
+
+  }
 
   return (
     <>
-      <form className={styles.forms}>
+      <form className={styles.forms} onSubmit={handleSubmit}>
         <input
           type="text"
           value={titles}
           onChange={(e) => setTitles(e.target.value)}
+          required
         />
 
         <textarea
           value={textArea}
           className={styles.textarea}
           onChange={(e) => setTextArea(e.target.value)}
+          required
         ></textarea>
-        <button type="submit" onClick={handleSubmit} >Submit</button>
+        <button type="submit">Submit</button>
       </form>
 
       <div className={styles.maincontent}>
         <ul>
           {notes.map((note) => (
-            <li key={note?.id}>
-              {editID !== note.id && <label>{note.title}</label>}
+            <li key={note.id}>
+              {editID !== note.id && (
+                <>
+                  <label>{note.title}</label>
+                  <span>{note.content}</span>
+                </>
+              )}
+              <button onClick={e => handleDelete(note.id)}>Delete</button>
             </li>
           ))}
         </ul>
